a86008703617d4affdae63bc9687c7098286289e
Samuel Wong <sandymule@gmail.com>
2022-10-24 16:42:37 -0700

    update to correspond with writeup

--- contracts/ZK.sol
+++ contracts/ZK.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.6.11;
 import "./InitVerifier.sol";
 
 contract ZK is Verifier {
+    event Minted(string message, address minter);
+
     uint256 public hash =
         uint256(
             15893827533473716138720882070731822975159228540693753428689375377280130954696
@@ -14,20 +16,20 @@ contract ZK is Verifier {
         owner = msg.sender;
     }
 
-    function isPreImage(
+    function mint(
         uint256[2] memory a,
         uint256[2] memory b_0,
         uint256[2] memory b_1,
         uint256[2] memory c,
         uint256[1] memory inputs // public inputs
-    ) public view returns (bool) {
-        require(hash == inputs[0], "wrong hash");
+    ) public returns (bool) {
+        // Without line below, security vuln
+        // require(hash == inputs[0], "wrong hash");
         uint256[1] memory publicInputs = inputs;
-        return verifyProof(a, [b_0, b_1], c, publicInputs);
-
-        // require(verifyProof(a, [b_0, b_1], c, publicInputs), "invalid proof");
-        // _mint();
-        // return true;
+        require(verifyProof(a, [b_0, b_1], c, publicInputs), "invalid proof");
+        _mint();
+        emit Minted("Mint successful", msg.sender);
+        return true;
     }
 
     function _mint() internal {}
