fb432d7aba7edeafb086a65c3c1fd9808ec8a4da
koalateectrl <koalateectrl@gmail.com>
2024-01-29 14:14:11 -0800

    comment out config

--- hardhat.config.js
+++ hardhat.config.js
@@ -25,14 +25,14 @@ module.exports = {
       },
     ],
   },
-  networks: {
-    hardhat: {},
-    sepolia: {
-      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-      accounts: [`${process.env.SEPOLIA_PRIVATE_KEY}`]
-    },
-  },
-  etherscan: {
-    apiKey: `${process.env.ETHERSCAN_API_KEY}`
-  }
+  // networks: {
+  //   hardhat: {},
+  //   sepolia: {
+  //     url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+  //     accounts: [`${process.env.SEPOLIA_PRIVATE_KEY}`]
+  //   },
+  // },
+  // etherscan: {
+  //   apiKey: `${process.env.ETHERSCAN_API_KEY}`
+  // }
 };
